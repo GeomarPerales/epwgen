cb90bd3c9e7558b0f1b151a560718a7ee98a1e6e
Geomar Perales Apaico <per3app@gmail.com>
2021-05-21 23:36:50 -0500

    Add files via upload

---
+++ R/blaney.criddle.R
@@ -0,0 +1,12 @@
+#' A list of light hours per day, maximum hours and extraterrestial radiation
+#'
+#' a list of 3 dataframes: first list is light hours per day, second list is maximum hours
+#' and thir list is extraterrestial radiation.
+#' @format A list of 3 dataframes.
+#' \describe{
+#'   \item{light.hours}{a dataframe of light hours (in percentage) per day by month and latitud}
+#'   \item{max.hours}{a dataframe of maximum hours (hours/day) by month and latitud}
+#'   \item{radiation}{a dataframe of extraterrestial radiation (mm/day) per day by month and latitud}
+#'   ...
+#' }
+"blaney.criddle"
